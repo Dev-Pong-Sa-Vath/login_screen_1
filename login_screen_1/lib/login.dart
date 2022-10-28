08780c6b075626089d30a112975c867f6092d737
Dev-Pong-Sa-Vath <Phongcawath2558@gmail.com>
2022-10-28 21:18:26 +0700

    Add TextField

---
+++ login_screen_1/lib/login.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.all(10),
+      child: ListView(children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(10),
+          child: Text(
+            'Login',
+            style: TextStyle(
+                color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 30),
+          ),
+        ),
+        Container(padding: EdgeInsets.fromLTRB(10, 10, 10, 0),child: TextField(obscureText: true,),)
+      ]),
+    ));
+  }
+}
